2656c2d12d2479dd37259da96d7b4a123e19663d
gabmartinbr <alu0101539157@ull.edu.es>
2026-04-29 11:07:14 +0000

    pe

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,32 @@
 import { GameDocumentInterface, Videogame } from "./models/Videogame.js";
 import "./db/mongoose.js"
 
+const nfs = new Videogame({
+  title: "nfs",
+  developer: "ea",
+  publisher: "ea",
+  genre: ["action", "adventure"],
+  platform: ["ps5", "pc"],
+  releaseDate: new Date(2021, 5, 1),
+  price: 70,
+  score: 90,
+  multiplayer: true,
+  dlcs: ["",]
+})
+
 const brawl = new Videogame({
-  title: "Brawl Stars",
-  developer: "Supercell",
-  publisher: "Supercell",
-  genre: ["action", "rpg", "adventure"],
-  platform: ["mobile", "pc"],
-  releaseDate: new Date(2022, 5, 1),
+  title: "brawl stars",
+  developer: "supercel",
+  publisher: "supercel",
+  genre: ["action"],
+  platform: ["mobile"],
+  releaseDate: new Date(2019, 5, 1),
   price: 0,
   score: 70,
   multiplayer: true,
   dlcs: ["",]
 })
+
 
 
 const createVideogame = (myGame: GameDocumentInterface): Promise<GameDocumentInterface> => {
@@ -44,25 +58,53 @@ const getById = (id: string): Promise<GameDocumentInterface> => {
       });
   })
 }
-// const getAllVideogames = (): Promise<GameDocumentInterface> => {
-//   return new Promise((resolve, reject) => {
-//     const videogames: Videogame[]
 
-//   })
-// }
+const deleteById = (id: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    Videogame.findByIdAndDelete(id).then(() => {
+      resolve()
+    }).catch((error) => {
+        console.error("No se pudo borrar el juego", error.message);
+        reject(error)
+      });
+  })
+}
 
-// createVideogame(brawl)
-// .then((game) => {
-//   console.log(`guardado ${game}`)
-// })
-// .catch((err) => {
-//   console.log(err)
-// })
+createVideogame(nfs)
+.then((game) => {
+  console.log(`guardado ${game}`)
+})
+.catch((err) => {
+  console.log(err)
+})
+createVideogame(brawl)
+.then((game) => {
+  console.log(`guardado ${game}`)
+})
+.catch((err) => {
+  console.log(err)
+})
 
-getById("69f1df2fc4af809c4cec9490")
+getById("69f1e598f40d1d060fcdd4b8")
 .then((game) => {
   console.log(`${game}`)
 })
 .catch((err) => {
   console.log(err)
 })
+
+getById("69f1e598f40d1d060fcdd4b9")
+.then((game) => {
+  console.log(`${game}`)
+})
+.catch((err) => {
+  console.log(err)
+})
+
+// deleteById("69f1e598f40d1d060fcdd4b9")
+// .then(() => {
+//   console.log(`borrado juego`)
+// })
+// .catch((err) => {
+//   console.log(err)
+// })
